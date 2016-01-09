98f6d1e28250c0be92b0f0867834c933e64656ae
v-reyder <v.reyder@2gis.ru>
2016-01-09 14:18:15 +0600

    Draw simple calendar

--- static/landing.js
+++ static/landing.js
@@ -14,13 +14,39 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 app.controller('LandingController', ['$scope', 'Service', 'NgTableParams',
     function ($scope, Service, NgTableParams) {
+        var weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Cб', 'Вc'];
+        $scope.week = getWeek();
+        $scope.times = getTimes();
+
+        function getTimes() {
+            var output = [];
+            var init = 9;
+            for (var i = 0; i < 9; i++) {
+                output.push(init + i + ':00')
+                output.push(init + i + ':30')
+            }
+            return output;
+        }
+
+        function getWeek() {
+            var now = new Date();
+            var week = [];
+            for (var i = 0; i < 7; i++) {
+                var date = new Date();
+                date.setDate(date.getDate() + i);
+                week.push(date);
+            }
+            return week;
+        }
+
+
         $scope.selectedServices = [];
 
          function isServiceSelected(service) {
             var out = _.find($scope.selectedServices, function (item) {
                 return item.id === service.id;
             });
-            if (out) { return true; } else {return false; }
+            if (out) { return true; } else { return false; }
          }
 
          function addServiceToSelected(item) {
